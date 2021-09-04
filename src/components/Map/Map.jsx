41686949dba0f87ea1cdc82bc4841ca2c786c77b
dane <dane.m.petersen@gmail.com>
2021-09-03 19:28:42 -0500

    playing around with leaflet and openweather api in temporary component of map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -9,19 +9,23 @@ const position = [51.505, -0.09]
 
 function Map() {
   return (
-    <div className="container">
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-    <Popup>
-    A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-    </Marker>
-    </MapContainer>,
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=11ec4ec7b29812e54c0f261032fbce7b`}
+      />
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=11ec4ec7b29812e54c0f261032fbce7b`}
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
